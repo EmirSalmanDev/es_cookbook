73759c0c6464ad9a7f1728165b4c5281652a7169
Emir Salman <emirsalman.info@gmail.com>
2025-03-08 20:14:42 +0300

    highlighted recipe added

--- es_cookbook/src/pages/Home.jsx
+++ es_cookbook/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 import "../styles/home.css";
+import Brownie from "../assets/brownie.jpg";
 
 function Home() {
   return (
@@ -27,6 +29,33 @@ function Home() {
             doloribus aut soluta ipsum obcaecati ex magnam, iusto, dolores amet.
           </p>
         </div>
+      </div>
+
+      <div className="highlighted_recipe">
+        <Container>
+          <Row>
+            <h2 className="highlighted_header">This Week's Recipe</h2>
+          </Row>
+          <Row>
+            <Col sm={3}>
+              <Image src={Brownie} fluid />
+            </Col>
+            <Col sm={9} className="highlighted_text">
+              <div>
+                <span className="date">March 8, 2025</span>
+                <h3>Best Ever Brownies</h3>
+              </div>
+              <p>
+                You know those times when you’re supposed to bring treats
+                somewhere but you seriously don’t have the time? MAKE THESE.
+                They are the answer to treats at your next office treat day,
+                birthday party, Bible study, basketball thingy, or Friday night
+                lounge party on the couch.
+              </p>
+              <a href="#">Go to the recipe</a>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </>
   );
