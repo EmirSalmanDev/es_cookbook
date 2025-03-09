175b1a4676bd0bd72f478a8a748e862be527cc0c
Emir Salman <emirsalman.info@gmail.com>
2025-03-09 12:50:50 +0300

    about page added

--- es_cookbook/src/pages/About.jsx
+++ es_cookbook/src/pages/About.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../styles/about.css";
 
 function About() {
-  return <div>About Me</div>;
+  return (
+    <>
+      <div className="about-me">
+        <Container>
+          <Row>
+            <Col>
+              <p>
+                This is just a small Bootstrap and React.js project, so the
+                website doesn’t have much for now.
+              </p>
+              <p>
+                This page is pretty simple and empty, but the recipes you'll
+                find here are the real ones I’ve actually used (and still do) in
+                real life. No unnecessary extras—just the dishes I genuinely
+                enjoy and cook. Feel free to try them out!
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 }
 
 export default About;
